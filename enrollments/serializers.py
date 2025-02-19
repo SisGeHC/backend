b7f28095dad66ef6593ea06fe24b1d742a69ca8f
brun0m <brun0m.sousa@aluno.uece.br>
2025-02-18 21:34:14 -0300

    ✨ Atualizações no backend: melhorias e novas funcionalidades

--- enrollments/serializers.py
+++ enrollments/serializers.py
@@ -4,20 +4,25 @@ from events.serializers import EventSerializer
 from students.serializers import StudentSerializer
 from students.models import Student
 from .models import Enrollment
-
+from django.conf import settings
 
 class EnrollmentSerializer(serializers.ModelSerializer):
-    student = serializers.PrimaryKeyRelatedField(queryset=Student.objects.all())  # ✅ Pegando Student corretamente
-    event = EventSerializer(read_only=True)
+    student = serializers.PrimaryKeyRelatedField(queryset=Student.objects.all())  
+    event = EventSerializer(read_only=True)  
+    qr_code_url = serializers.SerializerMethodField()  
 
     class Meta:
         model = Enrollment
-        fields = ["id", "student", "event", "qr_code", "attended"]
-        read_only_fields = ["qr_code", "attended"]
+        fields = ["id", "student", "event", "qr_code", "qr_code_url", "attended"] 
+        read_only_fields = ["qr_code", "qr_code_url", "attended"]
+
+    def get_qr_code_url(self, obj):
+        if obj.qr_code:
+            return f"{settings.MEDIA_URL}{obj.qr_code}"  
+        return None
 
 
-class CreateEnrollmentSerializer(ModelSerializer):
-
+class CreateEnrollmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Enrollment
         fields = ["id", "student", "event", "qr_code", "attended"]
